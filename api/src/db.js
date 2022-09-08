2004dbab43957a4b601ff8f70d17db2d85f34365
Zeus <zeusvargas26@gmail.com>
2022-09-07 22:34:09 -0500

    fix: add port import

--- api/src/db.js
+++ api/src/db.js
@@ -2,9 +2,9 @@ require('dotenv').config()
 const { Sequelize } = require('sequelize')
 const fs = require('fs')
 const path = require('path')
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_DB } = process.env
+const { PGUSER, PGPASSWORD, PGHOST, PGPORT, PGDATABASE} = process.env
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DB}`, {
+const sequelize = new Sequelize(`postgresql://${{ PGUSER }}:${{ PGPASSWORD }}@${{ PGHOST }}:${{ PGPORT }}/${{ PGDATABASE }}`, {
 	logging: false, // set to console.log to see the raw SQL queries
 	native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 })
